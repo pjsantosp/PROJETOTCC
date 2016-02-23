c498a6d5ed5bfd01fa2377c58df857e5da240004
Paulo J Santos <pjsantospaulo@gmail.com>
2016-02-23 17:25:08 -0400

    Criado Repository Cidade Estado

---
+++ Sptd.Web/Sptd.Web/Repositories/EstadoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sptd.Web.Models;
+
+namespace Sptd.Web.Repositories
+{
+    public class EstadoRepository
+    {
+        public long EstadoPadrao()
+        {
+            using (DbSPTD db = new DbSPTD())
+            {
+                long SiglaPadrao = db.Estado.Where(e => e.Sigla == "RO").First().IdEstado;
+                return SiglaPadrao;
+            }
+        }
+        public List<Estado> ObterEstados()
+        {
+            using (DbSPTD db = new DbSPTD())
+            {
+                return db.Estado.ToList();
+            }
+        }
+    }
+}
